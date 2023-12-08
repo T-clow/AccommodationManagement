9dd27d2a5510b8d745b08c32bfc71f7f2609a5ff
T-clow <813.danger@gmail.com>
2023-12-09 00:56:25 +0900

    ユーザー編集画面戻るリンク修正 編集画面個別作成

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,18 +1,27 @@
 # app/controllers/users/registrations_controller.rb
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :authenticate_user!, only: [:edit_email_password, :edit_profile]
   before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
-
+  before_action :configure_account_update_params, only: [:update, :edit_profile, :update_profile]
 
   def update
     super
   end
 
-   def after_update_path_for(resource)
+  def after_update_path_for(resource)
     root_path
   end
 
+  def edit_profile
+    @user = current_user
+  end
+
+  def update_profile
+    if current_user.update(account_update_params)
+      redirect_to account_path, notice: 'プロフィールが更新されました。'
+    else
+      render :edit_profile
+    end
+  end
 
   def destroy
     super
